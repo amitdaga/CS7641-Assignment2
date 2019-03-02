1a3734d580da650230a060016cfa00695b24ab76
Amit Daga <amitdaganit@gmail.com>
2019-03-02 22:11:11 +0530

    First commit

---
+++ run-all.sh
@@ -0,0 +1,9 @@
+/Users/adaga/jython2.7.0/bin/jython continuouspeaks.py
+/Users/adaga/jython2.7.0/bin/jython flipflop.py
+/Users/adaga/jython2.7.0/bin/jython tsp.py
+/Users/adaga/jython2.7.0/bin/jython NN-Backprop.py
+/Users/adaga/jython2.7.0/bin/jython NN-GA.py
+/Users/adaga/jython2.7.0/bin/jython NN-RHC.py
+/Users/adaga/jython2.7.0/bin/jython NN-SA.py
+python plotting.py
+
